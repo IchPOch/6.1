f47f2d1b5a671fcd0a5e62ee29a9011970fe6a9e
Миша Епринцев <meprincev@bk.ru>
2020-05-12 23:45:37 +0300

    123

---
+++ 6.1F/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace _6._1F
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.M = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.V = new System.Windows.Forms.TextBox();
+            this.N = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.VV = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.MM = new System.Windows.Forms.TextBox();
+            this.CH = new System.Windows.Forms.TextBox();
+            this.NN = new System.Windows.Forms.TextBox();
+            this.D = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // M
+            // 
+            this.M.AutoSize = true;
+            this.M.Location = new System.Drawing.Point(-1, 90);
+            this.M.Name = "M";
+            this.M.Size = new System.Drawing.Size(0, 13);
+            this.M.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Жми";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // V
+            // 
+            this.V.Location = new System.Drawing.Point(152, 21);
+            this.V.Multiline = true;
+            this.V.Name = "V";
+            this.V.Size = new System.Drawing.Size(265, 176);
+            this.V.TabIndex = 3;
+            // 
+            // N
+            // 
+            this.N.Location = new System.Drawing.Point(2, 67);
+            this.N.Name = "N";
+            this.N.Size = new System.Drawing.Size(134, 20);
+            this.N.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-1, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Введите размер массива";
+            // 
+            // MA
+            // 
+            this.MA.Location = new System.Drawing.Point(2, 114);
+            this.MA.Name = "MA";
+            this.MA.Size = new System.Drawing.Size(134, 20);
+            this.MA.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Введите число ";
+            // 
+            // VV
+            // 
+            this.VV.Location = new System.Drawing.Point(152, 243);
+            this.VV.Multiline = true;
+            this.VV.Name = "VV";
+            this.VV.Size = new System.Drawing.Size(265, 176);
+            this.VV.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Жми";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MM
+            // 
+            this.MM.Location = new System.Drawing.Point(2, 275);
+            this.MM.Name = "MM";
+            this.MM.Size = new System.Drawing.Size(134, 20);
+            this.MM.TabIndex = 12;
+            // 
+            // CH
+            // 
+            this.CH.Location = new System.Drawing.Point(2, 382);
+            this.CH.Name = "CH";
+            this.CH.Size = new System.Drawing.Size(134, 20);
+            this.CH.TabIndex = 13;
+            // 
+            // NN
+            // 
+            this.NN.Location = new System.Drawing.Point(2, 324);
+            this.NN.Name = "NN";
+            this.NN.Size = new System.Drawing.Size(134, 20);
+            this.NN.TabIndex = 14;
+            // 
+            // D
+            // 
+            this.D.AutoSize = true;
+            this.D.Location = new System.Drawing.Point(-1, 259);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(60, 13);
+            this.D.TabIndex = 15;
+            this.D.Text = "Введите m";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-1, 308);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Введите n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-1, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Введите число ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.D);
+            this.Controls.Add(this.NN);
+            this.Controls.Add(this.CH);
+            this.Controls.Add(this.MM);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.VV);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.N);
+            this.Controls.Add(this.V);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.M);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label M;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox V;
+        private System.Windows.Forms.TextBox N;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox MA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox VV;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox MM;
+        private System.Windows.Forms.TextBox CH;
+        private System.Windows.Forms.TextBox NN;
+        private System.Windows.Forms.Label D;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 6.1F/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6._1F
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int[] Input()
+        {
+            Random rnd = new Random();
+            int n = Convert.ToInt32(N.Text);
+            int[] mas = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = rnd.Next(0,100);
+            }
+            return mas;
+        }
+        private void Print(int[] mas)
+        {
+            {
+                for (int i = 0; i < mas.Length; ++i)
+                {
+                    V.Text += mas[i];
+                    V.Text += Environment.NewLine;
+                }
+            }
+        }
+        private void Change(int[] mas, int a)
+        {
+            V.Text += Environment.NewLine;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] > a)
+                    V.Text += " " + i;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int[] mas = Input();
+                V.Text = "";
+                int n = Convert.ToInt32(MA.Text);
+                Print(mas);
+                Change(mas, n);
+            }
+            catch
+            {
+                V.Text = "Неккоректно введённые данные";
+            }
+
+        }
+        private int[,] Input(out int n, out int m)
+        {
+            Random rnd = new Random();
+            n = int.Parse(NN.Text);
+            m = int.Parse(MM.Text);
+            int[,] a = new int[n, m];
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < m; ++j)
+                {
+                    a[i, j] = rnd.Next(0,100);
+                }
+            return a;
+        }
+        private void Print(int[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); ++i, VV.Text += Environment.NewLine)
+                for (int j = 0; j < a.GetLength(1); ++j)
+                    VV.Text += a[i, j] + "  ";
+        }
+        private void Change(int[,] a, int n)
+        {
+            VV.Text += Environment.NewLine;
+            for (int i = 0; i < a.GetLength(0); i++)
+                for (int j = 0; j < a.GetLength(1); ++j)
+                    if (a[i, j] > n)
+                        VV.Text += "["+i+","+ j+"]" + "  ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VV.Text = "";
+                int n = Convert.ToInt32(CH.Text);
+                int q, m;
+                int[,] mas = Input(out q, out m);
+                Print(mas);
+                Change(mas, n);
+            }
+            catch
+            {
+                VV.Text = "Неккоректно введены данные";
+            }
+        }
+    }
+}
